98756bc8bb19f6b608315b4ecd5bf7043df49509
Joppe Aarts <joppe@apestaartje.info>
2022-02-11 10:45:13 +0100

    Extended recipe list with more a11y features

--- src/component/recipe-list/RecipeListItem.jsx
+++ src/component/recipe-list/RecipeListItem.jsx
@@ -2,26 +2,38 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import { RecipeProp } from '../../prop-types/RecipeProp';
+import { Recipe } from '../recipe/Recipe';
 
 export function RecipeListItem({ recipe, isActive, toggleActive }) {
+  const id = `recipe-list-item-${recipe.id}`;
+  const detailId = `recipe-detail-${recipe.id}`;
+
   return (
     <li key={recipe.title} className="list-group-item">
       <h2 className="recipe-list__item__title">
         <button
           aria-expanded={isActive}
+          aria-controls={detailId}
           tabIndex={0}
+          id={id}
           onClick={() => toggleActive(recipe.id)}
         >
           {recipe.title}
         </button>
       </h2>
-      {isActive && <p>{recipe.description}</p>}
+
+      <Recipe
+        recipe={recipe}
+        id={detailId}
+        labelledby={id}
+        isHidden={!isActive}
+      />
     </li>
   );
 }
 
 RecipeListItem.propTypes = {
-  recipe: RecipeProp,
-  isActive: PropTypes.bool,
-  toggleActive: PropTypes.func,
+  recipe: RecipeProp.isRequired,
+  isActive: PropTypes.bool.isRequired,
+  toggleActive: PropTypes.func.isRequired,
 };
