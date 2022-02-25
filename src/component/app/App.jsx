fa0866df3c010c44a1f5b5fe6e3c6ecf6269dca2
Joppe Aarts <joppe@apestaartje.info>
2022-02-25 14:35:15 +0100

    render recipe list in App

--- src/component/app/App.jsx
+++ src/component/app/App.jsx
@@ -1,5 +1,19 @@
+import './app.css';
+
 import React from 'react';
 
+import { RecipeContextProvider } from '../../context/recipe/RecipeContext';
+import { RecipeList } from '../recipe-list/RecipeList';
+
 export function App() {
-  return <div>App</div>;
+  return (
+    <RecipeContextProvider>
+      <div className="container py-3">
+        <header className="d-flex justify-content-between align-items-center mb-3">
+          <h1 className="mb-3">Mijn kookboek</h1>
+        </header>
+        <RecipeList />
+      </div>
+    </RecipeContextProvider>
+  );
 }
